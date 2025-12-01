464e3037d5a59533545f04b2b4c90feae600ccb1
Jose-Marcello <josemarcellogardeldealemar@gmail.com>
2025-12-01 20:57:42 -0300

    Alterações no environments

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  // URL CORRIGIDA PARA O ACA (Backend)
-  apiUrl: 'https://api-bancodeitens-qa.thankfultree-b9485fed.brazilsouth.azurecontainerapps.io', 
-};   
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   // URL do backend que está no Railway:
   apiUrl: 'https://bancodeitens-v4-production.up.railway.app' 
 };
